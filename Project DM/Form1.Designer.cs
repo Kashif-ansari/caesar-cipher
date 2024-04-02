053a28dcac89d4f5b3532b60d79ad467f9fe3c29
Kashif-ansari <mkashifansari579@gmail.com>
2024-04-03 04:53:15 +0500

    push

---
+++ Project DM/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿
+namespace Project_DM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPlain = new System.Windows.Forms.TextBox();
+            this.txtCipherEnc = new System.Windows.Forms.TextBox();
+            this.txtCipherDec = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtShift = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtPlain
+            // 
+            this.txtPlain.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlain.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
+            this.txtPlain.Location = new System.Drawing.Point(275, 220);
+            this.txtPlain.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPlain.Multiline = true;
+            this.txtPlain.Name = "txtPlain";
+            this.txtPlain.Size = new System.Drawing.Size(377, 218);
+            this.txtPlain.TabIndex = 0;
+            this.txtPlain.TextChanged += new System.EventHandler(this.txtPlain_TextChanged);
+            // 
+            // txtCipherEnc
+            // 
+            this.txtCipherEnc.BackColor = System.Drawing.Color.White;
+            this.txtCipherEnc.Enabled = false;
+            this.txtCipherEnc.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCipherEnc.ForeColor = System.Drawing.Color.Blue;
+            this.txtCipherEnc.Location = new System.Drawing.Point(668, 220);
+            this.txtCipherEnc.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCipherEnc.Multiline = true;
+            this.txtCipherEnc.Name = "txtCipherEnc";
+            this.txtCipherEnc.Size = new System.Drawing.Size(380, 218);
+            this.txtCipherEnc.TabIndex = 1;
+            // 
+            // txtCipherDec
+            // 
+            this.txtCipherDec.BackColor = System.Drawing.Color.White;
+            this.txtCipherDec.Enabled = false;
+            this.txtCipherDec.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCipherDec.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.txtCipherDec.Location = new System.Drawing.Point(1063, 220);
+            this.txtCipherDec.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCipherDec.Multiline = true;
+            this.txtCipherDec.Name = "txtCipherDec";
+            this.txtCipherDec.Size = new System.Drawing.Size(373, 218);
+            this.txtCipherDec.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label1.Location = new System.Drawing.Point(549, 91);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(486, 51);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Techinque Caeser Cipher";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Location = new System.Drawing.Point(325, 186);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Message (PLAIN TEXT)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(798, 186);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Encrypted";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(1196, 187);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Decrypted";
+            // 
+            // txtShift
+            // 
+            this.txtShift.Location = new System.Drawing.Point(487, 498);
+            this.txtShift.Margin = new System.Windows.Forms.Padding(4);
+            this.txtShift.Name = "txtShift";
+            this.txtShift.Size = new System.Drawing.Size(164, 22);
+            this.txtShift.TabIndex = 8;
+            this.txtShift.TextChanged += new System.EventHandler(this.txtShift_TextChanged);
+            this.txtShift.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtShift_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(325, 500);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 25);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "CODE : ";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.BackColor = System.Drawing.Color.DeepPink;
+            this.btnEncrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEncrypt.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncrypt.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnEncrypt.Location = new System.Drawing.Point(729, 446);
+            this.btnEncrypt.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(267, 55);
+            this.btnEncrypt.TabIndex = 10;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = false;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.BackColor = System.Drawing.Color.DeepPink;
+            this.btnDecrypt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDecrypt.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecrypt.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnDecrypt.Location = new System.Drawing.Point(1108, 446);
+            this.btnDecrypt.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(267, 55);
+            this.btnDecrypt.TabIndex = 11;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = false;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.DeepPink;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnClear.Location = new System.Drawing.Point(729, 509);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(267, 55);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.DeepPink;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnExit.Location = new System.Drawing.Point(1108, 509);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(267, 55);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DeepPink;
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1455, 75);
+            this.panel1.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label6.Location = new System.Drawing.Point(387, 11);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(638, 48);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "CIPHER ENCRYPTION/DECRYPTION";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.ClientSize = new System.Drawing.Size(1455, 652);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtShift);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCipherDec);
+            this.Controls.Add(this.txtCipherEnc);
+            this.Controls.Add(this.txtPlain);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPlain;
+        private System.Windows.Forms.TextBox txtCipherEnc;
+        private System.Windows.Forms.TextBox txtCipherDec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtShift;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Project DM/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_DM
+{
+    public partial class Form1 : Form
+    {
+        public int shift;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string Encryption(string txt,int key)
+        {
+            string plainCapital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string plainSmall = "abcdefghijklmnopqrstuvwxyz ";
+            this.shift = key;
+            string cipher = "";
+            char space = ' '; 
+            for (int i= 0;i<txt.Length;i++)
+            {
+                if(char.IsUpper(txt[i]))
+                {
+                    for (int j = 0; j < plainCapital.Length; j++)
+                    {
+                        if (txt[i] == plainCapital[j])
+                        {
+                            cipher = cipher + plainCapital[(j + shift) % 26];
+                        }
+                    }
+                }
+                else if (txt[i] == space)
+                {
+                    cipher = cipher + " ";
+                }
+                else
+                {
+                    for (int j = 0; j < plainSmall.Length; j++)
+                    {
+                        if (txt[i] == plainSmall[j])
+                        {
+                            cipher = cipher + plainSmall[(j + shift) % 26];
+                        }
+                    }
+                }
+            }
+            return cipher;
+        }
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (txtPlain.Text != "" && txtShift.Text != "" && txtPlain.Text != null && txtShift.Text != null)
+            {
+                txtCipherEnc.Text = Encryption(txtPlain.Text, Convert.ToInt32(txtShift.Text));
+            }
+            else
+            {
+                MessageBox.Show("Please Enter Plain Text or Code Number To Proceed Further", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void combobox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtPlain.Enabled = true;
+            txtShift.Enabled = true;
+            
+            
+        }
+        private string Decryption(string txt)
+        {
+            string plainCapital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string plainSmall = "abcdefghijklmnopqrstuvwxyz";
+            string CipherDec = "";
+            char space = ' ';
+            for (int i = 0; i < txt.Length; i++)
+            {
+                if (char.IsUpper(txt[i]))
+                {
+                    for (int j = 0; j < plainCapital.Length; j++)
+                    {
+                        if (txt[i] == plainCapital[j])
+                        {
+                            if (j >= shift)
+                            {
+                                CipherDec = CipherDec + plainCapital[(j - shift) % 26];
+                            }
+                            else
+                            {
+                                int NonNegativeIndex = ((shift-j) % 26);
+                                CipherDec = CipherDec + plainCapital[26-NonNegativeIndex];
+                            }
+                            
+                        }
+                    }
+                }
+                else if (txt[i] == space)
+                {
+                    CipherDec = CipherDec + " ";
+                }
+                else
+                {
+                    for (int j = 0; j < plainSmall.Length; j++)
+                    {
+                        if (txt[i] == plainSmall[j])
+                        {
+                            if (j >= shift)
+                            {
+                                CipherDec = CipherDec + plainSmall[(j - shift) % 26];
+                            }
+                            else
+                            {
+                                int NonNegativeIndex = ((shift - j) % 26);
+                                CipherDec = CipherDec + plainSmall[26 - NonNegativeIndex];
+                            }
+
+                        }
+                    }
+                }
+            }
+            return CipherDec;
+        }
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (txtCipherEnc.Text!=null && txtCipherEnc.Text!="")
+            {
+                txtCipherDec.Text = Decryption(txtCipherEnc.Text);
+            }
+            else
+            {
+                MessageBox.Show("To Proceed Further You Need Cipher Text", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCipherDec.Text = "";
+            txtCipherEnc.Text = "";
+            txtPlain.Text = "";
+            txtShift.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtShift_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!(Char.IsDigit(ch)) && ch != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtPlain_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtShift_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
